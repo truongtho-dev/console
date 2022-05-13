306bd67e5ca909efb73a0707dff473808b31197b
Tho Nguyen <tho.nguyen@transwap.com>
2022-05-13 10:02:01 +0700

    init repo

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp
+{
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			
+			Console.ReadKey();
+		}
+
+
+	}
+
+
+}
